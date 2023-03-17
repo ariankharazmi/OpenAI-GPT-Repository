f5fbee397b164f76ae444408dc27d6f49143d0c5
ariankharazmi <100003892+ariankharazmi@users.noreply.github.com>
2023-03-17 22:00:01 +0000

    03-17-2023 Upload

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
 import os
 import openai
 
-# Load your API key from an environment variable or secret management service
-openai.api_key = os.getenv("")
+import dataclasses
+import logging
+import math
+import io
+import sys
+import time
+import json
+from typing import Optional, Sequence, Union
 
-response = openai.Completion.create(model="text-davinci-003", prompt="Say this is a test", temperature=0, max_tokens=7)
+import tqdm
+import copy
+# Load your API key from an environment variable or secret management service
+API_KEY = ''
+openai.api_key = API_KEY
+
+
+response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt="Tell me about OpenAI",
+    temperature=1.0,
+    max_tokens=1000,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0)
+
+
+print(response)
+
